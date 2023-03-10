1da6e292b5329befd3800bca9d26dc7cc57f60b5
Raef-Hany <98807473+Raef-Hany@users.noreply.github.com>
2023-03-10 11:37:09 +0200

    validation function implementation
    I implemented the validation function, but I had to use a data structure called sets, because it has built in functions that allowed me to manipulate the user's input, but it is just for this function. everything else will be in vectors as we decided.

--- QM_implementation/bool_function.cpp
+++ QM_implementation/bool_function.cpp
@@ -1,5 +1,39 @@
 #include "bool_function.h"
 
+bool bool_function::validate(string x) {
+    int i = 0;
+    vector<bool> User_given_function;
+
+    set<char> vars;  // Set to keep track of input variables
+
+    bool has_consecutive = false;
+    char prev = '\0';
+
+    for (char c : x) 
+    {
+        if (isalpha(c)) {
+            if (prev == c) {  // Check if there are consecutive duplicate variables
+                has_consecutive = true;
+                //return false;
+            }
+            vars.insert(c);  // Add variable to set
+        } else if (c != '+' && c != '`') {  // Check if input contains only valid characters
+            cout << "Error: Invalid character '" << c << "' found in input." << endl;
+            return false;
+        }
+        prev = c;
+    }
+
+    if (has_consecutive) {  // Print an error message for consecutive duplicate variables
+        cout << "Error: Consecutive duplicate variables found in input." << endl;
+        return false;
+    }
+
+    // If we reach this point, the input string is valid
+    cout << "The input string is valid." << endl;
+    return true;
+}
+
 void bool_function::gen_table()
 {
 	int size = literals.size();
